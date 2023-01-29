b4383e1c9a96118e3b178a6f35d89833969547f1
BlazejA <58550726+BlazejA@users.noreply.github.com>
2023-01-29 21:22:10 +0100

    Add users management

---
+++ Library/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Library.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public ICollection<BookModel> RentedBooks { get; set; }
+
+    }
+}
--- Library/Repository/Context/LibraryContext.cs
+++ Library/Repository/Context/LibraryContext.cs
@@ -10,4 +10,5 @@ public class LibraryContext : DbContext
     }
 
     public DbSet<BookModel> Books { get; set; }
+    public DbSet<UserModel> Users { get; set; }
 }
